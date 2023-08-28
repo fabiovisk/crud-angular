e80ecf31445baa9f50d3667da1cae088cb7cf25b
fabio <fabiormiranda@hotmail.com>
2023-08-28 16:10:57 -0300

    first commit

---
+++ src/app/courses/courses/courses.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent {
+
+  courses: Course[] = [
+    { _id: '1', name: 'Angular', category: 'Front-End' }
+  ];
+  displayedColumns = [ 'name', 'category' ];
+
+}
