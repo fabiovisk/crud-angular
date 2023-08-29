90bfe060a37cf9f09a2da8dd1fcbdc1f20fca185
fabio <fabiormiranda@hotmail.com>
2023-08-29 19:39:03 -0300

    second commit front-end courses list

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,16 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { catchError, Observable, of } from 'rxjs';
+
 import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
+
 export class CoursesComponent {
 
-  courses: Course[] = [
-    { _id: '1', name: 'Angular', category: 'Front-End' }
-  ];
+  courses$: Observable<Course[]>;
   displayedColumns = [ 'name', 'category' ];
+
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+  ) {
+    this.courses$ = this.coursesService.list()
+    .pipe(
+      catchError( error => {
+        console.log('Erro ao carregar cursos.');
+        return of([])
+      })
+    );
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
+  }
+
 
 }
